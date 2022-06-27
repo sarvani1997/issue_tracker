ac121f9d2bac0629eea6f60afc2a27eb994137c6
Sarvani Sanaboyina <80517819+sarvani1997@users.noreply.github.com>
2022-06-28 00:28:16 +0530

    create user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,31 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { trpc } from "../utils/trpc";
+import React from "react";
+
+const UserCreator = () => {
+  const inputRef = React.useRef<HTMLInputElement | null>(null);
+  const client = trpc.useContext();
+  const { mutate, isLoading } = trpc.useMutation("git.create", {
+    onSuccess: () => {
+      client.invalidateQueries(["git.get-all-users"]);
+      if (!inputRef.current) return;
+      inputRef.current.value = "";
+    },
+  });
+  return (
+    <input
+      ref={inputRef}
+      disabled={isLoading}
+      onKeyDown={(event) => {
+        if (event.key === "Enter") {
+          mutate({ name: event.currentTarget.value });
+          event.currentTarget.value = "";
+        }
+      }}
+    ></input>
+  );
+};
 
 const Home: NextPage = () => {
   const { data, isLoading } = trpc.useQuery(["git.get-all-users"]);
@@ -12,6 +37,7 @@ const Home: NextPage = () => {
       {data.map((user) => {
         return <div>{user.name}</div>;
       })}
+      <UserCreator />
     </>
   );
 };
--- src/server/router/git.ts
+++ src/server/router/git.ts
@@ -2,8 +2,21 @@ import { createRouter } from "./context";
 import { z } from "zod";
 import { prisma } from "../db/client";
 
-export const gitRouter = createRouter().query("get-all-users", {
-  async resolve() {
-    return await prisma.user.findMany();
-  },
-});
+export const gitRouter = createRouter()
+  .query("get-all-users", {
+    async resolve() {
+      return await prisma.user.findMany();
+    },
+  })
+  .mutation("create", {
+    input: z.object({
+      name: z.string().min(5).max(500),
+    }),
+    async resolve({ input }) {
+      return await prisma.user.create({
+        data: {
+          name: input.name,
+        },
+      });
+    },
+  });
