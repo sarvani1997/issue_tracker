dd40c73e3212bc1f0578f6f9fa1ff0a6bbd6d026
Sarvani Sanaboyina <80517819+sarvani1997@users.noreply.github.com>
2022-06-28 00:47:33 +0530

    create commentRouter

---
+++ src/server/router/comment.ts
@@ -0,0 +1,29 @@
+import { createRouter } from "./context";
+import { z } from "zod";
+import { prisma } from "../db/client";
+
+export const commentRouter = createRouter()
+  .query("get-all-comments", {
+    input: z.object({ issueId: z.string() }),
+    async resolve({ input }) {
+      return await prisma.comment.findMany({
+        where: {
+          issueId: input.issueId,
+        },
+      });
+    },
+  })
+  .mutation("create-comment", {
+    input: z.object({
+      comment: z.string().min(5).max(5000),
+      issueId: z.string(),
+    }),
+    async resolve({ input }) {
+      return await prisma.comment.create({
+        data: {
+          comment: input.comment,
+          issueId: input.issueId,
+        },
+      });
+    },
+  });
