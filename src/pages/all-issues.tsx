a3e2cfa926d9f9fc00260ee0f82479b78f4c4508
Sarvani Sanaboyina <80517819+sarvani1997@users.noreply.github.com>
2022-06-28 14:37:14 +0530

    implement all issues page

---
+++ src/pages/all-issues.tsx
@@ -0,0 +1,39 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { trpc } from "../utils/trpc";
+import React from "react";
+import Router from "next/router";
+import Link from "next/link";
+
+const Issues = () => {
+  const { data, isLoading } = trpc.useQuery(["issue.get-all-issues"]);
+  const { data: userData, isLoading: userLoading } = trpc.useQuery([
+    "user.get-all-users",
+  ]);
+  if (isLoading || !data) return <div>Loading ....</div>;
+  if (userLoading || !userData) return <div>Loading ....</div>;
+
+  let user = localStorage.getItem("user");
+  if (!user) {
+    Router.push("/");
+  }
+
+  return (
+    <>
+      {data.map((issue) => {
+        let userName = userData.find((user) => user.id === issue.userId)?.name;
+
+        return (
+          <div key={issue.id}>
+            <Link href={`/issue/${issue.id}`}>
+              <a>{issue.title}</a>
+            </Link>
+            <span>Created by {userName}</span>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default Issues;
