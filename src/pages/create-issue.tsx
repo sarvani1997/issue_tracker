90a0cdf5d525950242de3fb888c99a1f8bca372d
Sarvani Sanaboyina <80517819+sarvani1997@users.noreply.github.com>
2022-07-05 18:12:07 +0530

    implement form to create ui

--- src/pages/create-issue.tsx
+++ src/pages/create-issue.tsx
@@ -1,37 +1,59 @@
-import type { NextPage } from "next";
-import Head from "next/head";
 import { trpc } from "../utils/trpc";
-import React from "react";
-import Link from "next/link";
 import Router from "next/router";
+import { useForm } from "react-hook-form";
+import { useState, useEffect } from "react";
 
 const IssueCreator = () => {
-  const { mutate, isLoading } = trpc.useMutation("issue.create-issue", {
-    onSuccess: () => {
-      Router.push("/my-issues");
-    },
-  });
-  let [userId] = React.useState(() => {
+  let [userId] = useState(() => {
     if (typeof localStorage !== "undefined") {
       return localStorage.getItem("user");
     }
   });
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (!userId) {
       Router.push("/");
     }
+  }, []);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { mutate, isLoading } = trpc.useMutation("issue.create-issue", {
+    onSuccess: () => {
+      Router.push("/my-issues");
+    },
   });
+
+  const onSubmit = (data) => {
+    mutate({ title: data.title, userId, description: data.description });
+  };
+
   return (
-    <input
-      disabled={isLoading}
-      onKeyDown={(event) => {
-        if (event.key === "Enter") {
-          mutate({ title: event.currentTarget.value, userId });
-          event.currentTarget.value = "";
-        }
-      }}
-    ></input>
+    <div className="bg-zinc-200 flex flex-col p-6 min-h-screen">
+      <h1 className="text-2xl text-blue-600 font-bold mb-6">Create an Issue</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          disabled={isLoading}
+          className="input input-bordered w-full rounded-md p-3 mb-6"
+          placeholder="Title"
+          {...register("title", { required: true })}
+        ></input>
+        <textarea
+          className="input input-bordered w-full rounded-md p-3 mb-6"
+          placeholder="add description"
+          rows="5"
+          {...register("description", { required: true })}
+        ></textarea>
+        <button
+          className="outline text-blue-600 outline-offset-2 outline-blue-600 rounded-md px-4 hover:bg-blue-600 hover:text-white"
+          type="submit"
+        >
+          Create an issue
+        </button>
+      </form>
+    </div>
   );
 };
 
